298164cb56e5fc846ada90e70b854fdab7c4a282
Administrator <Administrator@YMYBN4W0J39GN31>
2019-06-28 18:21:15 +0800

    这是commons

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,15 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//这是一个测试3
+//u给iu后i就【建瓯avc
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	private Integer id;
+	private String name;
+	private Integer number;
+}
